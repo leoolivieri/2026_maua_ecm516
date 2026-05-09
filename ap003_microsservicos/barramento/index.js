4ac9f235deecd61fa760040391cbf0cf63792eac
leoolivieri <olivierileo30@gmail.com>
2026-05-09 09:27:48 -0300

    learn(ap01-ms516): Encerra codigo 4.3.13

---
+++ ap003_microsservicos/barramento/index.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+
+app.post('/eventos', async function   (req, res) {
+    const evento = req.body
+    console.log('Evento recebido:', evento)
+    try {
+        await axios.post('http://localhost:4000/eventos', evento)
+        axios.post('http://localhost:5001/eventos', evento)
+        res.status(201).send(evento)
+    }
+    catch(e) {}
+    try {
+        await  axios.post('http://localhost:5001/eventos', evento)
+    }
+    catch(e) {}
+    res.end()
+})
+
+//colocar  barramento de eventos em funcionamento na porta 10000
+const port = 10000
+app.listen(port, () => {
+    console.log(`Barramento de eventos. Porta ${port}.`)
+})
+
---
+++ ap003_microsservicos/lembretes/index.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const lembretes = {}
+let contador = 0
+
+// responde requisições de leitura e consulta
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes) // retorna todos os lembretes
+})
+
+// responde requisições de criação: é chamado quando quer cirar ou enviar um dado novo
+app.post('/lembretes', async (req, res) => {
+    contador++
+    const { texto } = req.body // pega o texto enviado pelo cliente
+    lembretes[contador] = {contador, texto} // salva no objeto
+
+    // emite evento para barramento de eventos
+    // porta 10000 é onde o barramento de eventos está rodando
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: 'LembreteCriado',
+        dados: {contador, texto} // equivale a { contador: 1, texto: "Fazer café" }
+    })
+    // uso do async await: o código só continua depois que a requisição for feita, ou seja, depois que o evento for enviado para o barramento de eventos
+    //Ordem garantida — o res.send só executa depois que o axios terminar
+
+    res.status(200).send(lembretes[contador]) // retorna o lembrete criado
+})
+
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000.')
+})
