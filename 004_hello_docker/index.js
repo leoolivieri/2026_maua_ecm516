4ba5e45d329890d2bf1b148db0913f1541f3f45e
leoolivieri <olivierileo30@gmail.com>
2026-08-15 06:33:18 -0600

    learn(ap-01 mss): Inicia topico "Implantacao"

---
+++ 004_hello_docker/index.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const app = express()
+app.use(express.json())
+
+app.get('/hey-docker', (req,res) => {
+    res.json({
+        mensagem: "Hey, Docker!"
+    })
+})
+
+app.listen(5200, () => console.log('up and runnig inside Docker'))
+
